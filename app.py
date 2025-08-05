fbf8f0a325fac19bdf8bfee1cae9cb6003d6c8e6
Hibandd122 <130156106+Hibandd122@users.noreply.github.com>
2025-08-05 12:40:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 urls = {
     "vn88": "https://vn88ve.com",
     "m88": "https://bet88ve.com",
