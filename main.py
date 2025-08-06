e79bf57855e1c387475ccf89aeabe49e7c028c88
Hibandd122 <130156106+Hibandd122@users.noreply.github.com>
2025-08-06 14:21:30 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+urls = {
+    "vn88": "https://vn88zu.com",
+    "m88": "https://bet88ve.com",
+    "fb88": "https://fb88dq.com",
+    "bk8": "https://bk8hn.com",
+    "v9bet": "http://v9betlg.com",
+    "188bet": "https://88betux.com",
+    "w88": "https://w88abc.com",
+    "fun88": "https://fun88de.com",
+    "w88 w88xlm": "https://w88xlm.com"
+}
+
+@app.route("/get-url", methods=["POST"])
+def get_url():
+    data = request.json
+    site_name = data.get("site", "").lower().strip()
+
+    for name, url in urls.items():
+        if site_name in name.lower():
+            return jsonify(url)  # chỉ trả về chuỗi URL
+
+    return "Site not found", 404
