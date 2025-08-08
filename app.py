ab03ee9b6069504e1121dd33317d23534bdd7931
Hibandd122 <130156106+Hibandd122@users.noreply.github.com>
2025-08-08 19:05:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def get_url():
     return jsonify({"error": "Site not found"}), 404
 @app.route('/Bypass.js', methods=["GET"])
 def bypass_js():
-    return send_from_directory('static', 'Bypass.js')
+    return send_from_directory('static', 'Bypass.js', mimetype='application/javascript')
